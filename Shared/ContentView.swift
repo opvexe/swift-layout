f7aa0710605c1d3179337f56ef1fc6ab06cf6a09
taoshumin_vendor <taoshumin@foxmail.com>
2022-02-22 21:58:39 +0800

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by taoshumin_vendor on 2022/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/circleApp.swift
@@ -0,0 +1,17 @@
+//
+//  circleApp.swift
+//  Shared
+//
+//  Created by taoshumin_vendor on 2022/2/22.
+//
+
+import SwiftUI
+
+@main
+struct circleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
